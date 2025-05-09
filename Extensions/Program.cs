26ae275974e70d9e31d27489e3cbac3b09181c5d
Oli Sturm <oliver+commitsigning@oliversturm.com>
2025-05-09 15:04:01 +0100

    feat: basic example for Extensions in C# 14 (.NET 10)

---
+++ Extensions/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Extensions {
+  public static class Enumerable {
+    extension<T>(IEnumerable<T> source) {
+        public void OlisExtra() {
+          Console.WriteLine("Executing Oli's extra function");
+        }
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      int[] numbers = [1, 2, 3, 4, 5];
+      numbers.OlisExtra();
+    }
+  }
+}
