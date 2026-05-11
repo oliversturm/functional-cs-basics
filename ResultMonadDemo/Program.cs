74839a93ba7267b6ad405a6e899894b34bb98054
Oli Sturm <oliver+commitsigning@oliversturm.com>
2026-05-11 21:49:50 +0100

    error type fixes

--- ResultMonadDemo/Program.cs
+++ ResultMonadDemo/Program.cs
@@ -7,18 +7,21 @@ public abstract record Error {
 }
 
 internal class Program {
-  private static Result<int, Error.Maths> Divide(int a, int b) {
+  private static Result<int, Error> Divide(int a, int b) {
     return b != 0 ? Ok(a / b) : Fail(new Error.Maths("Division by zero"));
   }
 
-  private static Result<int, Error.Maths> Add(int a, int b) => Ok(a + b);
-  private static Result<int, Error.Maths> Subtract(int a, int b) => Ok(a - b);
-  private static Result<int, Error.Maths> Multiply(int a, int b) => Ok(a * b);
+  private static Result<int, Error> Add(int a, int b) => Ok(a + b);
+  private static Result<int, Error> Subtract(int a, int b) => Ok(a - b);
+  private static Result<int, Error> Multiply(int a, int b) => Ok(a * b);
 
   private static void Process(int start) =>
     Add(start, start)
+      //.Log("add", "Result")
       .Bind(x => Subtract(x, 28))
+      //.Log("subtract", "Result")
       .Bind(x => Multiply(x, 3))
+      //.Log("multiply", "Result")
       .Bind(x => Divide(100, x))
       .Switch(
         x => Console.WriteLine($"Result: {x}"),
